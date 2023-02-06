7eba587d77c0962822a82d7d9c3973cdd3264d5b
ben5186 <ben@acsiri.com>
2023-02-06 14:01:00 -0500

    Add Board.java method headers

--- Game2048/src/Board.java
+++ Game2048/src/Board.java
@@ -1,4 +1,32 @@
 // 2-5-23
 public class Board {
-
+	private int[][] table;
+	
+	public Board(int x, int y) {
+		table = new int[x][y];
+	}
+	public Board() {
+		table = new int[4][4];
+	}
+	
+	public void moveUp() {
+		
+	}
+	public void moveDown() {
+		
+	}
+	public void moveRight() {
+		
+	}
+	public void moveLeft() {
+		
+	}
+	
+	public void newNumbers() {
+		
+	}
+	
+	public void merge(int thisx, int thisy, int otherx, int othery) {
+		
+	}
 }
