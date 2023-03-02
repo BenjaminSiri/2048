c9b98f7ec341859a839f23340030f36c7bdbed1d
ben5186 <ben@acsiri.com>
2023-03-02 01:20:24 -0500

    Added stack class

--- Game2048/src/Stack.java
+++ Game2048/src/Stack.java
@@ -1,5 +1,24 @@
 // 3-1-23
 public class Stack {
-
+	private Node head;
+	
+	public Stack() {
+		head = null;
+	}
+	
+	public void setHead(Node node) {
+		head = node;
+	}
+	
+	public void push(Node node) {
+		node.setNext(head.getNext());
+		setHead(node);
+	}
+	
+	public int pop() {
+		int outValue = head.getValue();
+		head.setNext(head.getNext());
+		return outValue;
+	}
 
 }
