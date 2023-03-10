ea4dfd4f1d23a56e26ef59ebe307f209557f5144
ben5186 <ben@acsiri.com>
2023-03-10 02:30:09 -0500

    Gui grid created

---
+++ Game2048/src/GUI.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+
+public class GUI extends JFrame{
+	
+	int spacing = 10;
+	
+	public GUI() {
+		this.setTitle("2048 test");
+		this.setSize(400, 435);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setResizable(false);
+		
+		Panel panel = new Panel();
+		this.setContentPane(panel);
+	}
+	
+	public class Panel extends JPanel{
+		public void paintComponent(Graphics g) {
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(0, 0, 400, 400);
+			g.setColor(Color.GRAY);
+			int size = 90;
+			for (int i=0; i<4; i++) {
+				for (int j=0; j<4; j++) {
+					g.fillRect((spacing+i*size)+20, (spacing+j*size)+20, size-2*spacing, size-2*spacing);
+				}
+			}
+		}
+	}
+}
--- Game2048/src/Test3.java
+++ Game2048/src/Test3.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.*;
+import java.awt.*;
 import javax.swing.*;
 
-public class Test3 {
+public class Test3 implements Runnable{
+	
+	GUI gui = new GUI();
+	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("2048 Test");
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(4,4));
-		frame.setSize(400, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		new Thread(new Test3()).start();
+		
 	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			gui.repaint();
+		}
+	}
+	
+	
 }
